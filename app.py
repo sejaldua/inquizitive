69fc668adb7be3b74ec7041bfeb16d31fd7c83d2
Sejal Dua <sejaldua@gmail.com>
2023-09-22 12:39:09 -0400

    improve UI of chatbot

--- app.py
+++ app.py
@@ -378,19 +378,36 @@ if 'materials_processed' in st.session_state and st.session_state['materials_pro
         # We will get the user's input by calling the get_text function
         def get_text():
             global input_text
-            input_text = st.text_input("Ask a question! ", key="input", on_change=clear_input_text)
+            input_text = st.chat_input("Ask a question!")
             return input_text
 
         user_input = get_text()
 
+        # Initialize chat history
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                if message['content'] != user_input:
+                    st.markdown(message["content"])
+
         if user_input:
             output = chat.answer(user_input, docsearch)
-            st.session_state.past.append(user_input)
-            st.session_state.generated.append(output)
+            st.session_state.messages.append({"role": "user", "content": user_input})
+            # Display user message in chat message container
+            with st.chat_message("user"):
+                st.markdown(user_input)
 
-        if st.session_state['generated']:
-            for i in range(len(st.session_state['generated'])-1, -1, -1):
-                message(st.session_state["generated"][i], key=str(i))
-                message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                full_response = ""
+                for char in output:
+                    full_response += char
+                    message_placeholder.markdown(full_response + "▌")
+                message_placeholder.markdown(full_response)
 
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
 
